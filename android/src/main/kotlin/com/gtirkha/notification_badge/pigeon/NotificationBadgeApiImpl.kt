ec41428f70c4c8fdd328d100214e49dbd4c5a0b1
gktirkha <gktirkha@gmail.com>
2026-05-15 09:41:52 +0530

    refactor(android): migrate plugin to Pigeon API from MethodChannel

---
+++ android/src/main/kotlin/com/gtirkha/notification_badge/pigeon/NotificationBadgeApiImpl.kt
@@ -0,0 +1,7 @@
+package com.gtirkha.notification_badge.pigeon
+
+class NotificationBadgeApiImpl:NotificationBadgeApi {
+    override fun setCount(callback: (Result<Boolean>) -> Unit) {
+        TODO("Not yet implemented")
+    }
+}
