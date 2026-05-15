2d3b6fb547b1b66124ecad0d6afb2bd95733fc52
gktirkha <gktirkha@gmail.com>
2026-05-15 11:07:12 +0530

    feat(ios): preserve badge count when app enters background

--- ios/Classes/NotificationBadgePlugin.swift
+++ ios/Classes/NotificationBadgePlugin.swift
@@ -1,19 +1,38 @@
 import Flutter
 import UIKit
+import UserNotifications
 
 public class NotificationBadgePlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "notification_badge", binaryMessenger: registrar.messenger())
+    let api = NotificationBadgeApiImpl()
+    NotificationBadgeApiSetup.setUp(binaryMessenger: registrar.messenger(), api: api)
+
     let instance = NotificationBadgePlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
+    NotificationCenter.default.addObserver(
+      instance,
+      selector: #selector(applicationDidEnterBackground),
+      name: UIApplication.didEnterBackgroundNotification,
+      object: nil
+    )
   }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
+  @objc private func applicationDidEnterBackground() {
+    DispatchQueue.main.async {
+      let count = UIApplication.shared.applicationIconBadgeNumber
+      guard count > 0 else { return }
+      if #available(iOS 16.0, *) {
+        UNUserNotificationCenter.current().setBadgeCount(count) { error in
+          if let error = error {
+            print("[NotificationBadge] Error preserving badge count in background: \(error.localizedDescription)")
+          }
+        }
+      } else {
+        UIApplication.shared.applicationIconBadgeNumber = count
+      }
     }
+  }
+
+  deinit {
+    NotificationCenter.default.removeObserver(self)
   }
 }
